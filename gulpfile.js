dc2f9d07da4f85d5555e1c1d15e85fbd46f235d2
Caleb Nance <calebnance@gmail.com>
2020-03-25 23:23:19 -0400

    adding json data to html

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
+const data = require('gulp-data');
 const del = require('del');
+const fs = require('fs');
 const gulp = require('gulp');
 const nunjucksRender = require('gulp-nunjucks-render');
+const path = require('path');
 const sass = require('gulp-sass');
 
 // set reload from browserSync
@@ -14,7 +17,7 @@ const prod = environment === 'production';
 const directory = prod ? 'static_prod' : 'static_dev';
 
 // task handling per environment
-const sharedTasks = ['clean', 'fonts', 'sass', 'compileHTML'];
+const sharedTasks = ['clean', 'fonts', 'images', 'sass', 'compileHTML'];
 const devTasks = ['serve'];
 const prodTasks = [];
 
@@ -34,8 +37,46 @@ gulp.task('sass', () => {
 });
 
 gulp.task('compileHTML', () => {
+  const defaultData = require('./src/html/data/default.json');
+  console.log('defaultData', defaultData);
+
   return gulp
     .src('./src/html/pages/**/*.nunjucks')
+    .pipe(
+      data((file) => {
+        // set path to json file, specific to the HTML page we are compiling!
+        const fileName = path.basename(file.path, '.nunjucks');
+        const pathToFile = `./src/html/data/${fileName}.json`;
+        // let pageData = {};
+
+        console.log('pathToFile', pathToFile);
+        console.log('------------------------------');
+
+        // does data file exist?
+        if (fs.existsSync(pathToFile)) {
+          // delete cache, we always want the latest json data..
+          delete require.cache[require.resolve(pathToFile)];
+          // log that we are grabbing data
+          console.log('grabbing data from: ' + pathToFile);
+          // grab specific page data
+          const pageData = require(pathToFile);
+          console.log('data exists', pageData);
+          console.log('======================');
+          console.log('======================');
+          return pageData;
+        } else {
+          // const pageData = defaultData;
+          console.log('data does not', defaultData);
+          console.log('======================');
+          console.log('======================');
+          return defaultData;
+        }
+        // set canonical
+        // pageData.canonical = `${config.urlBase}${fileName}.html`;
+
+        // return pageData;
+      })
+    )
     .pipe(
       nunjucksRender({
         path: './src/html/templates',
@@ -46,6 +87,10 @@ gulp.task('compileHTML', () => {
 
 gulp.task('fonts', () => {
   return gulp.src('./src/fonts/**/*').pipe(gulp.dest(`./${directory}/fonts`));
+});
+
+gulp.task('images', () => {
+  return gulp.src('./src/images/**/*').pipe(gulp.dest(`./${directory}/images`));
 });
 
 // static server
@@ -60,7 +105,7 @@ gulp.task('serve', () => {
   });
 
   // watch for changes to specific files and re-compile
-  gulp.watch('src/html/**/*.nunjucks', gulp.parallel('compileHTML'));
+  gulp.watch('src/html/**/*.(json|nunjucks)', gulp.parallel('compileHTML'));
   gulp.watch('src/scss/**/*.scss', gulp.parallel('sass'));
 
   // on directory change, reload browserSync
